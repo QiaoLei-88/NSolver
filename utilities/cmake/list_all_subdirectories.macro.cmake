94e67607df44958b485d98d581af3a1b66c9b280
Lei Qiao <qiaol618@gmail.com>
2015-04-03 21:19:45 -0500

    Automate the pickup of test cases

---
+++ utilities/cmake/list_all_subdirectories.macro.cmake
@@ -0,0 +1,34 @@
+MACRO(LIST_ALL_SUBDIRECTORIES result curdir)
+
+  FILE(GLOB __children RELATIVE ${curdir} ${curdir}/*)
+
+  SET(__dir_list "")
+  FOREACH(__child ${__children})
+    IF(IS_DIRECTORY ${curdir}/${__child})
+      LIST(APPEND __dir_list ${__child})
+      LIST(SORT __dir_list)
+    ENDIF()
+  ENDFOREACH()
+  UNSET(__child)
+
+  SET(${result} ${__dir_list})
+  UNSET(__dir_list)
+  
+ENDMACRO()
+
+
+MACRO(PICKUP_ALL_SUBDIRECTORIES curdir)
+
+  SET(__sub_dir_list "")
+  LIST_ALL_SUBDIRECTORIES(__sub_dir_list ${curdir})
+
+  FOREACH(__sub_dir ${__sub_dir_list})
+    ADD_SUBDIRECTORY(${__sub_dir})
+  ENDFOREACH()
+
+   UNSET(__sub_dir)
+   UNSET(__sub_dir_list)
+
+  ENABLE_TESTING()
+
+ENDMACRO()
---
+++ utilities/cmake/setup_test_case.macro.cmake
@@ -0,0 +1,62 @@
+MACRO(SETUP_TEST_CASE)
+# The execution flow of this macro is driven by dependency relationship 
+# and runs from bottom to top.
+
+  ENABLE_TESTING()
+
+  # Prepair both shared and exclusive input files for running test case.
+  # Also accumulate denpence file list.
+  SET (_input_file_list_full "")
+
+  FOREACH (__file ${_shared_input_file_list})
+    ADD_CUSTOM_COMMAND(OUTPUT  ${_test_directory}/${__file}
+      COMMAND ${_exec_copy}  ${_para_copy} ${_shared_input_dir}/${__file} ${_test_directory}/${__file}
+      WORKING_DIRECTORY
+        ${_test_directory}
+      VERBATIM
+      )
+    LIST(APPEND _input_file_list_full ${_test_directory}/${__file})
+  ENDFOREACH(__file)
+  UNSET(__file)
+
+  FOREACH (__file ${_input_file_list})
+    ADD_CUSTOM_COMMAND(OUTPUT  ${_test_directory}/${__file}
+      COMMAND ${_exec_copy}  ${_para_copy} ${_test_src_directory}/${__file} ${_test_directory}/${__file}
+      WORKING_DIRECTORY
+        ${_test_directory}
+      VERBATIM
+      )
+    LIST(APPEND _input_file_list_full ${_test_directory}/${__file})
+  ENDFOREACH(__file)
+  UNSET(__file)
+
+  # Run test case
+  ADD_CUSTOM_COMMAND(OUTPUT ${_test_directory}/${_output_file}
+    COMMAND  ${_exec_run} ${_para_run}
+    WORKING_DIRECTORY
+      ${_test_directory}
+    DEPENDS
+      ${_input_file_list_full}
+      ${_test_src_directory}/${_comparison_file}
+      ${PRJ_NAME} 
+    VERBATIM
+    )
+
+  # Compare output
+  ADD_CUSTOM_TARGET(diff_${_test_name}
+    COMMAND ${_exec_diff} ${_para_diff} ${_test_directory}/${_output_file}  ${_test_src_directory}/${_comparison_file}
+    DEPENDS
+      ${_test_directory}/${_output_file}
+      ${_test_src_directory}/${_comparison_file}
+    VERBATIM
+    )
+
+  # Finally add test target. This will be invoked by the ctest command.
+  ADD_TEST(NAME ${_test_name}
+    # The next command is equavalent to run 'make diff_${_test_name}' inside
+    # ${CMAKE_BINARY_DIR}.
+    COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target diff_${_test_name}
+    WORKING_DIRECTORY ${_test_directory}
+    )
+
+ENDMACRO()
