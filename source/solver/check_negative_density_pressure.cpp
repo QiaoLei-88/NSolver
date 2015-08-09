3bd8c4989526dbdb42982d946fe36a377cf86645
Lei Qiao <qiaol618@gmail.com>
2015-08-09 17:57:00 -0500

    move NSolver::check_negative_density_pressure to separated file

---
+++ source/solver/check_negative_density_pressure.cpp
@@ -0,0 +1,40 @@
+//
+//  NSolver::check_negative_density_pressure.cpp
+//
+//  Created by Lei Qiao on 15/8/9.
+//  A work based on deal.II turorial step-33.
+//
+
+#include <NSolver/solver/NSolver.h>
+
+namespace NSFEMSolver
+{
+  using namespace dealii;
+
+  template <int dim>
+  void NSolver<dim>::check_negative_density_pressure() const
+  {
+    FEValues<dim> fe_v (mapping, fe, quadrature, update_values);
+    const unsigned int   n_q_points = fe_v.n_quadrature_points;
+    std::vector<Vector<double> > solution_values (n_q_points,
+                                                  Vector<double> (dim+2));
+    typename DoFHandler<dim>::active_cell_iterator
+    cell = dof_handler.begin_active(),
+    endc = dof_handler.end();
+    for (; cell!=endc; ++cell)
+      if (cell -> is_locally_owned())
+        {
+          fe_v.reinit (cell);
+          fe_v.get_function_values (current_solution, solution_values);
+          for (unsigned int q=0; q<n_q_points; ++q)
+            {
+              const double density = solution_values[q] (EquationComponents<dim>::density_component);
+              AssertThrow (density > 0.0, ExcMessage ("Negative density encountered!"));
+              const double pressure = solution_values[q] (EquationComponents<dim>::pressure_component);
+              AssertThrow (pressure > 0.0, ExcMessage ("Negative pressure encountered!"));
+            }
+        }
+  }
+
+#include "NSolver.inst"
+}
