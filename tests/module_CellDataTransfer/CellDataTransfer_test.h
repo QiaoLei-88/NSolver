a27c2377c08a4c89ed95a11cb03910a6e1cf11c8
Lei Qiao <qiaol618@gmail.com>
2015-10-14 19:25:58 -0500

    add regTest category module_CellDataTransfer

---
+++ tests/module_CellDataTransfer/CellDataTransfer_test.h
@@ -0,0 +1,13 @@
+#ifndef __CellDataTransfer_test_h__
+#define __CellDataTransfer_test_h__
+
+#include <deal.II/grid/grid_generator.h>
+#include <NSolver/CellDataTransfer.h>
+
+#include <fstream>
+#include <iostream>
+
+using namespace dealii;
+using namespace NSFEMSolver;
+
+#endif
