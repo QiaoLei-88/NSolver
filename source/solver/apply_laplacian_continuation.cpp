d80ba767c80de8badc1d72301c321e889e75a94a
Lei Qiao <qiaol618@gmail.com>
2015-11-06 11:48:46 -0600

    use function calc_laplacian_indicator

--- source/solver/apply_laplacian_continuation.cpp
+++ source/solver/apply_laplacian_continuation.cpp
@@ -30,12 +30,19 @@ namespace NSFEMSolver
 
     std::vector<std::vector<Tensor<1,dim> > > grad_W (n_q_points,
                                                       std::vector<Tensor<1,dim> > (EquationComponents<dim>::n_components));
+    unsigned int n_laplacian = 0;
     typename DoFHandler<dim>::active_cell_iterator
     cell = dof_handler.begin_active(),
     endc = dof_handler.end();
     for (; cell!=endc; ++cell)
       if (cell->is_locally_owned())
         {
+          const bool use_laplacian = laplacian_indicator[cell->active_cell_index()] >= laplacian_threshold;
+          n_laplacian += use_laplacian;
+          const double factor = use_laplacian
+                                ? 1.0
+                                : 0.01;
+
           fe_v.reinit (cell);
           cell->get_dof_indices (dof_indices);
           fe_v.get_function_gradients (current_solution, grad_W);
@@ -45,7 +52,7 @@ namespace NSFEMSolver
               const unsigned int c = fe_v.get_fe().system_to_component_index (i).first;
               for (unsigned int q=0; q<n_q_points; ++q)
                 {
-                  residual += laplacian_coefficient *
+                  residual += laplacian_coefficient * factor *
                               (grad_W[q][c] *
                                fe_v.shape_grad_component (i, q, c)) *
                               fe_v.JxW (q);
@@ -57,7 +64,7 @@ namespace NSFEMSolver
                     {
                       for (unsigned int q=0; q<n_q_points; ++q)
                         {
-                          matrix_row[j]  += laplacian_coefficient *
+                          matrix_row[j]  += laplacian_coefficient * factor *
                                             (fe_v.shape_grad_component (j, q, c)
                                              * fe_v.shape_grad_component (i, q, c)) *
                                             fe_v.JxW (q);
@@ -69,6 +76,9 @@ namespace NSFEMSolver
               right_hand_side (dof_indices[i]) -= residual;
             }
         }
+    pcout << "n_laplacian = "
+          << Utilities::MPI::sum (n_laplacian, mpi_communicator)
+          << std::endl;
 
     return;
   }
